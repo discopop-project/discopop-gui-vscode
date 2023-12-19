254c3004309850cb6ca89d25006f484f7bbec7c2
goerlibe <23436477+goerlibe@users.noreply.github.com>
2023-12-19 12:07:37 +0100

    feat(optimizer): trigger optimizer run from configuration context menu

---
+++ src/Optimizer/OptimizerRunner.ts
@@ -0,0 +1,95 @@
+import { exec, execSync } from 'child_process'
+
+export enum OptimizerExecutionType {
+    Exhaustive = 'exhaustive',
+    Evolutionary = 'evolutionary',
+}
+
+export interface OptimizerOptions {
+    executionType?: OptimizerExecutionType
+    verbose?: boolean
+    doallMicrobenchFile?: string
+    reductionMicrobenchFile?: string
+    systemConfigurationFile?: string
+}
+
+export const DefaultOptimizerOptions: OptimizerOptions = {
+    executionType: OptimizerExecutionType.Evolutionary,
+    verbose: false,
+    doallMicrobenchFile: undefined,
+    reductionMicrobenchFile: undefined,
+    systemConfigurationFile: undefined,
+}
+
+export abstract class OptimizerRunner {
+    private constructor() {
+        throw new Error('This class cannot be instantiated')
+    }
+
+    private static buildCommand(options: OptimizerOptions): string {
+        let command = `discopop_optimizer`
+
+        switch (options.executionType) {
+            case OptimizerExecutionType.Evolutionary:
+                break // default, does not need to be specified
+            case OptimizerExecutionType.Exhaustive:
+                command += ' --exhaustive'
+                break
+            default:
+                throw new Error(
+                    `Unknown execution type: ${options.executionType}`
+                )
+        }
+
+        if (options.verbose) {
+            command += ' --verbose'
+        }
+
+        if (options.doallMicrobenchFile) {
+            command += ` --doall-microbench-file ${options.doallMicrobenchFile}`
+        }
+
+        if (options.reductionMicrobenchFile) {
+            command += ` --reduction-microbench-file ${options.reductionMicrobenchFile}`
+        }
+
+        if (options.systemConfigurationFile) {
+            command += ` --system-configuration ${options.systemConfigurationFile}`
+        }
+
+        return command
+    }
+
+    public static async run(
+        dotDiscoPoP: string,
+        {
+            executionType = OptimizerExecutionType.Evolutionary,
+            verbose = false,
+            doallMicrobenchFile = undefined,
+            reductionMicrobenchFile = undefined,
+            systemConfigurationFile = undefined,
+        }: OptimizerOptions
+    ): Promise<void> {
+        // might throw
+        const command = OptimizerRunner.buildCommand({
+            executionType,
+            verbose,
+            doallMicrobenchFile,
+            reductionMicrobenchFile,
+            systemConfigurationFile,
+        })
+        const stdout = execSync(command, {
+            cwd: dotDiscoPoP,
+            env: {
+                ...process.env,
+                DOT_DISCOPOP: dotDiscoPoP,
+            },
+            encoding: 'utf-8',
+        })
+
+        console.log('optimizer finished:')
+        console.log(stdout)
+
+        return
+    }
+}
