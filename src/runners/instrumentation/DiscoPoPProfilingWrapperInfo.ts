b755c8ccdfbca81904e9a1b8fe8e9cd55569ad5f
goerlibe <23436477+goerlibe@users.noreply.github.com>
2024-01-24 09:25:05 +0100

    feat: use discopop_cmake instead of finding the script manually

--- src/runners/instrumentation/DiscoPoPProfilingWrapperInfo.ts
+++ src/runners/instrumentation/DiscoPoPProfilingWrapperInfo.ts
@@ -6,71 +6,54 @@ export class DiscoPoPProfilingWrapperInfo implements WrapperInfo {
     private _configProvider: DiscoPoPConfigProvider =
         new DiscoPoPConfigProvider()
 
-    private _cmakeWrapper: Promise<string> | undefined = undefined
     public get cmakeWrapper(): Promise<string> {
-        if (this._cmakeWrapper === undefined) {
-            this._cmakeWrapper = this._configProvider.buildDirectory.then(
-                (build) => build + '/scripts/CMAKE_wrapper.sh'
-            )
-        }
-        return this._cmakeWrapper
+        return Promise.resolve('discopop_cmake')
     }
 
-    // TODO use promises for all of these
-    private _ccWrapper: string | undefined = undefined
-    public get ccWrapper(): string {
-        if (this._ccWrapper === undefined) {
-            this._ccWrapper =
-                this._configProvider.buildDirectory + '/scripts/CC_wrapper.sh'
-        }
-        return this._ccWrapper
+    public get ccWrapper(): Promise<string> {
+        return Promise.resolve('discopop_cc')
     }
 
-    private _cxxWrapper: string | undefined = undefined
-    public get cxxWrapper(): string {
-        if (this._cxxWrapper === undefined) {
-            this._cxxWrapper =
-                this._configProvider.buildDirectory + '/scripts/CXX_wrapper.sh'
-        }
-        return this._cxxWrapper
+    public get cxxWrapper(): Promise<string> {
+        return Promise.resolve('discopop_cxx')
     }
 
-    private _ldWrapper: string | undefined = undefined
-    public get ldWrapper(): string {
+    private _ldWrapper: Promise<string> | undefined = undefined
+    public get ldWrapper(): Promise<string> {
         if (this._ldWrapper === undefined) {
-            this._ldWrapper =
-                this._configProvider.buildDirectory +
-                '/scripts/LINKER_wrapper.sh'
+            this._ldWrapper = this._configProvider.buildDirectory.then(
+                (build) => build + '/scripts/LINKER_wrapper.sh'
+            )
         }
         return this._ldWrapper
     }
 
-    private _mpiccWrapper: string | undefined = undefined
-    public get mpiccWrapper(): string {
+    private _mpiccWrapper: Promise<string> | undefined = undefined
+    public get mpiccWrapper(): Promise<string> {
         if (this._mpiccWrapper === undefined) {
-            this._mpiccWrapper =
-                this._configProvider.buildDirectory +
-                '/scripts/MPI_CC_wrapper.sh'
+            this._mpiccWrapper = this._configProvider.buildDirectory.then(
+                (build) => build + '/scripts/MPI_CC_wrapper.sh'
+            )
         }
         return this._mpiccWrapper
     }
 
-    private _mpicxxWrapper: string | undefined = undefined
-    public get mpicxxWrapper(): string {
+    private _mpicxxWrapper: Promise<string> | undefined = undefined
+    public get mpicxxWrapper(): Promise<string> {
         if (this._mpicxxWrapper === undefined) {
-            this._mpicxxWrapper =
-                this._configProvider.buildDirectory +
-                '/scripts/MPI_CXX_wrapper.sh'
+            this._mpicxxWrapper = this._configProvider.buildDirectory.then(
+                (build) => build + '/scripts/MPI_CXX_wrapper.sh'
+            )
         }
         return this._mpicxxWrapper
     }
 
-    private _mpildWrapper: string | undefined = undefined
-    public get mpildWrapper(): string {
+    private _mpildWrapper: Promise<string> | undefined = undefined
+    public get mpildWrapper(): Promise<string> {
         if (this._mpildWrapper === undefined) {
-            this._mpildWrapper =
-                this._configProvider.buildDirectory +
-                '/scripts/MPI_LINKER_wrapper.sh'
+            this._mpildWrapper = this._configProvider.buildDirectory.then(
+                (build) => build + '/scripts/MPI_LINKER_wrapper.sh'
+            )
         }
         return this._mpildWrapper
     }
